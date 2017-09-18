0e22d9052944a0a514b8b5b95cbb9ddeef843b46
forDream <gam2046@gmail.com>
2017-09-18 13:35:25 +0800

    [jd-self-support] Fix #1 调整代码结构/优化筛选逻辑
    + 单页内实现翻页查询商品
    + 实现页脚替换与恢复
    + 调整代码结构
    * 部分分类检索页不支持单页内翻页查询,eg:https://list.jd.com/list.html?cat=12218,13581,13583

--- jd-self-support.user.js
+++ jd-self-support.user.js
@@ -8,62 +8,133 @@
 // @run-at      document-idle
 // @updateURL   https://github.com/gam2046/userscript/raw/master/jd-self-support.user.js
 // @supportURL  https://github.com/gam2046/userscript/issues/new
-// @version     0.2.1
+// @version     0.3.1
 // ==/UserScript==
 
 (function () {
     'use strict';
 
-    var selected = false;
-    var liTagName = "fd_show_jd_self_support";
-    var aTagName = "fd_shown_jd_self_support_link";
-
-    function findJdSelfSupport() {
-        console.log("beginning to find jd self support goods.");
-        var items = $(".gl-item");
-        var count = 0;
-        for (var n = 0; n < items.length; n++) {
-            var item = items[n];
-            if (selected) { // 选中对话框
-                if (item.innerHTML.match("//item\.jd\.com/\\d{6,7}.html") == null) {
-                    item.hidden = true;
-                    count++;
-                    console.log("Hidden index of " + n);
+    var extension = {
+        extensionSwitch: false,
+        selfGoodsCount: 0,
+        originalPager: undefined,
+        bottomPageText: undefined,
+        pagerText: function () { return "当前已加载" + SEARCH.current_page + "页    共计" + SEARCH.adv_param.page_count + "页"; },
+        const: {
+            aTagName: "fd_shown_jd_self_support_link",
+            liTagName: "fd_shown_jd_self_support"
+        },
+        enableExtension: function () { extension.extensionSwitch = true; },
+        disableExtension: function () { extension.extensionSwitch = false; },
+        initialization: function () {
+            window.setInterval(extension.funcAddSwitchbox, 2000);
+            window.setInterval(extension.funcFilterResult, 2000);
+        },
+        scrollPosition: function () {
+            return document.documentElement.scrollTop / document.documentElement.scrollHeight;
+        },
+        funcFilterResult: function () {
+            console.log("beginning to find jd self support goods.");
+            var items = document.getElementsByClassName("gl-item");
+            var count = 0;
+            for (var n = 0; n < items.length; n++) {
+                var item = items[n];
+                if (extension.extensionSwitch) { // 选中对话框
+                    if (item.innerHTML.match("//item\.jd\.com/\\d{6,7}.html") == null) {
+                        item.hidden = true;
+                        count++;
+                        console.log("Hidden index of " + n);
+                    }
+                } else {
+                    item.hidden = false;
                 }
+            }
+
+            extension.selfGoodsCount = items.length - count;
+
+            console.log("Total items " + items.length + " blocked " + count);
+        },
+        funcFindSwitchbox: function () {
+            var ul = document.getElementById("J_feature");
+            if (!ul) ul = document.getElementsByClassName("f-feature")[0];
+            return ul.children[0];
+        },
+        funcAddSwitchbox: function () {
+            if (document.getElementById(extension.const.liTagName) != null) {
+                document.getElementById(extension.const.aTagName).className = extension.extensionSwitch ? "selected" : "";
+                return;
+            }
+
+            var ul = extension.funcFindSwitchbox();
+            var li = document.createElement("li");
+            var a = document.createElement("a");
+
+            a.id = extension.const.aTagName;
+            a.href = "javascript:;";
+            a.className = extension.extensionSwitch ? "selected" : "";
+            a.innerHTML = "<i></i>仅显示京东自营商品";
+            a.onclick = extension.funcSwitchboxClick;
+
+            li.id = extension.const.liTagName;
+            li.appendChild(a);
+            ul.appendChild(li);
+        },
+        funcSwitchboxClick: function () {
+            extension.extensionSwitch = !extension.extensionSwitch;
+            extension.funcAddSwitchbox();
+            extension.funcModifyPagerJumper();
+        },
+        funcRequestGoods: function () {
+            var totalPage = SEARCH.adv_param.page_count;
+            var currentPage = SEARCH.current_page;
+
+            if (currentPage < totalPage) {
+                SEARCH.scroll();
+                extension.bottomPageText.innerText = extension.pagerText();
             } else {
-                item.hidden = false;
+                alert("已经到达最后一页，无法继续翻页");
+            }
+        },
+        funcModifyPagerJumper: function () {
+            var pageWrap = document.getElementById("J_bottomPage");
+            var modifyTagId = "fd_bottomPage";
+
+            if (extension.extensionSwitch) {
+                if (!SEARCH || !SEARCH.scroll) return; // 部分页面没有该对象则不修改页脚
+                // modify
+                pageWrap.hidden = true;
+
+                var modifyBottom = document.createElement("div");
+                modifyBottom.id = modifyTagId;
+                modifyBottom.className = "p-wrap";
+                document.getElementsByClassName("page clearfix")[0].appendChild(modifyBottom);
+
+                var pagerNum = document.createElement("span");
+                pagerNum.className = "p-num";
+                modifyBottom.appendChild(pagerNum);
+
+                var span = document.createElement("span");
+                span.innerText = extension.pagerText();
+                span.className = "p-skip";
+                extension.bottomPageText = span;
+
+                var nextButton = document.createElement("a");
+                nextButton.innerHTML = "<em>加载下一页</em><i>&gt;</i>";
+                nextButton.className = "pn-next";
+                nextButton.href = "javascript:;";
+                nextButton.onclick = extension.funcRequestGoods;
+
+                pagerNum.appendChild(span);
+                pagerNum.appendChild(nextButton);
+            } else {
+                // restore
+                pageWrap.hidden = false;
+
+                var modifyBottom = document.getElementById(modifyTagId);
+                if (modifyBottom) modifyBottom.remove();
             }
         }
+    };
 
-        console.log("Total items " + items.length + " blocked " + count);
-    }
-
-    function addCheckbox() {
-        if (document.getElementById(liTagName) != null) {
-            document.getElementById(aTagName).className = selected ? "selected" : "";
-            return;
-        }
-
-        var ul = document.getElementById("J_feature").children[0];
-        var li = document.createElement("li");
-        var a = document.createElement("a");
-
-        a.id = aTagName;
-        a.href = "javascript:;";
-        a.className = selected ? "selected" : "";
-        a.innerHTML = "<i></i>仅显示京东自营商品";
-        a.onclick = clickCheckbox;
-
-        li.id = liTagName;
-        li.appendChild(a);
-        ul.appendChild(li);
-    }
-
-    function clickCheckbox() {
-        selected = !selected;
-        addCheckbox();
-    }
-
-    window.setInterval(findJdSelfSupport, 2000);
-    window.setInterval(addCheckbox, 2000);
+    extension.initialization();
 })();
