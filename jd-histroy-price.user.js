d1aa05395f0c76c5b937b52d4307d7c64b55b6b0
forDream <gam2046@users.noreply.github.com>
2018-01-28 17:33:43 +0800

    Update jd-histroy-price.user.js
    修改URL适配模式，某些页面进入商品详情也会带有查询参数，导致无法匹配

--- jd-histroy-price.user.js
+++ jd-histroy-price.user.js
@@ -4,11 +4,11 @@
 // @namespace   https://github.com/gam2046/userscript
 // @description Shown histroy price of JD
 // @description:zh-CN [无广告]一目了然显示京东商城历史价格，没有其他额外功能。Chrome 64.+中测试通过，其他环境不保证可用。
-// @include     *://item.jd.com/*.html
+// @include     *://item.jd.com/*
 // @require     https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.bundle.min.js
 // @updateURL   https://github.com/gam2046/userscript/raw/master/jd-histroy-price.user.js
 // @supportURL  https://github.com/gam2046/userscript/issues/new
-// @version     1
+// @version     2
 // @grant       GM_xmlhttpRequest
 // @copyright   2018+, forDream <gan2046#gmail.com>
 // @author      forDream
